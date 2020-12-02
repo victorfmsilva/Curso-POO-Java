6af150b6e33a0b9139c8a0942303349cadebeb65
victorfmsilva <victormatoscps@gmail.com>
2020-12-02 15:04:00 -0300

    Curso POO Java #03b

---
+++ src/Aula03B/Aula03.java
@@ -0,0 +1,14 @@
+package Aula03B;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIG CRISTAL";
+        c1.cor = "Azul";
+//        c1.ponta = 0.5;
+        c1.carga = 80;
+//        c1.tampada = true;
+        c1.destampar();
+        c1.status();
+    }
+}
---
+++ src/Aula03B/Caneta.java
@@ -0,0 +1,34 @@
+package Aula03B;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    public void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("ERRO! A caneta está tampada.");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+
+    protected void tampar() {
+        this.tampada = true;
+    }
+
+    protected void destampar(){
+        this.tampada = false;
+    }
+
+}
