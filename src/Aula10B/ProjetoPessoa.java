f860f468bc63486a1c77f8e1b55945ea211fabb3
victorfmsilva <victormatoscps@gmail.com>
2020-12-10 18:58:09 -0300

    Curso POO Java #10b - Herança (Parte 1)

---
+++ src/Aula10B/ProjetoPessoa.java
@@ -0,0 +1,29 @@
+package Aula10B;
+
+public class ProjetoPessoa {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+
+
+        p1.setNome("Joao");
+        p2.setNome("Maria");
+        p3.setNome("Claudio");
+        p4.setNome("FAbiana");
+
+        p1.setSexo("M");
+        p4.setSexo("F");
+        p2.setIdade(18);
+
+        p2.setCurso("Informatica");
+        p3.setSalario(2.500f);
+        p4.setSetor("Estoque");
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+    }
+}
